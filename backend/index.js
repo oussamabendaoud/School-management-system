f43b1c626d207836389fc6d918afa988a29ae328
oussamabendaoud <oussamabendaoud111@gmail.com>
2024-07-31 13:53:58 +0100

    la configuration du backend

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config({ path: './config.env' });
 
-const app = express();
-const port = process.env.PORT || 4000;
+import app from "./app.js";
 
-// Middleware pour parser les requêtes JSON
-app.use(express.json());
-
-// Ligne de débogage
-console.log('MONGO_URL:', process.env.MONGO_URL);
-
-// Connexion à MongoDB
-const mongoURI = process.env.MONGO_URL;
-if (!mongoURI) {
-  console.error('MONGO_URL is not defined in the environment variables.');
-  process.exit(1);
-}
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
-
-// Route de base
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.listen(process.env.PORT, () => {
+    console.log( `Server listening on port ${process.env.PORT} `);
 });
 
-// Lancer le serveur
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+
